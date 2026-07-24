93d122ea91665358aa62738d761c1e1b1184968b
touhidcse <touhidcucse38@gmail.com>
2026-07-24 22:28:09 +0600

    project structure done

---
+++ app/(dashboardGroup)/dashboard/my-posts/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserMyPostPage = () => {
+  return (
+    <div>UserMyPostPage</div>
+  )
+}
+
+export default UserMyPostPage
---
+++ app/(dashboardGroup)/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const DashboardLayout = (
+    {
+        children
+    }:{
+        children: React.ReactNode
+    }
+) => {
+  return (
+    <>
+    {children}
+    </>
+  )
+}
+
+export default DashboardLayout
